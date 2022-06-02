4f93de54884e85bf0c2a85ebc2f455889b64033a
Raul Mamedov <raul.mamedov@devim.team>
2022-06-02 12:23:12 +0300

    add readme , fix code

--- src/test/java/com/example/XlsxApplicationTests.java
+++ src/test/java/com/example/XlsxApplicationTests.java
@@ -1,13 +1,76 @@
 package com.example;
 
+import com.example.exeption.StorageFileNotFoundException;
+import com.example.service.StorageService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class XlsxApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private StorageService storageService;
+
+
 	@Test
-	void contextLoads() {
+	public void shouldListAllFiles() throws Exception {
+		given(this.storageService.loadAll())
+				.willReturn(Stream.of(Paths.get("first.xlsx"), Paths.get("second.xlsx")));
+
+		this.mvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(model().attribute("files",
+						Matchers.contains("http://localhost/files/first.xlsx",
+								"http://localhost/files/second.xlsx")));
+	}
+
+	@Test
+	public void shouldSaveUploadedFile() throws Exception {
+		MockMultipartFile multipartFile = new MockMultipartFile("file", "test.xlsx",
+				"xlsx/plain", "Spring Framework".getBytes());
+		this.mvc.perform(multipart("/").file(multipartFile))
+				.andExpect(status().isFound())
+				.andExpect(header().string("Location", "/"));
+
+		then(this.storageService).should().store(multipartFile);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void should404WhenMissingFile() throws Exception {
+		given(this.storageService.loadAsResource("test.xlsx"))
+				.willThrow(StorageFileNotFoundException.class);
+
+		this.mvc.perform(get("/files/test.xlsx")).andExpect(status().isNotFound());
 	}
 
 }
+
+
